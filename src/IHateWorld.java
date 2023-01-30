7ec25d4941b0b1164ff6937cc1b7331e35f1b471
carlosblancoj <cblancojaner@cifpfbmoll.eu>
2023-01-30 16:43:39 +0100

    Push edited file

--- src/IHateWorld.java
+++ src/IHateWorld.java
@@ -1,7 +1,5 @@
 public class IHateWorld {
     public static void main(String[] args) {
         System.out.println("I dont hate world");
-        Math.random();
-        System.out.println("I hate world!");
     }
 }
