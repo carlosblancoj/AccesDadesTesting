50df102909b6ea0f7b84571b92cd31fb9c934313
zhijunlin7 <zlin1@cifpfbmoll.eu>
2023-01-30 16:26:28 +0100

    test merge

--- src/IHateWorld.java
+++ src/IHateWorld.java
@@ -1,5 +1,7 @@
 public class IHateWorld {
     public static void main(String[] args) {
+        System.out.println("I dont hate world");
+        Math.random();
         System.out.println("I hate world!");
     }
 }
